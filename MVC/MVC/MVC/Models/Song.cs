ccee5053eaf0eefc857f9662620a9dce7ae06639
inf136711 <jakub.r.gorny@student.put.poznan.pl>
2020-05-24 23:15:00 +0200

    Task 8: ASP.NET MVC

---
+++ MVC/MVC/MVC/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Song
+    {
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Artist is required!")]
+        [StringLength(50, ErrorMessage = "Maximal length of the name of a artist is 50 characters!")]
+        public string Artist { get; set; }
+        public string GenreId { get; set; }
+        public int Id { get; set; }
+    }
+}
